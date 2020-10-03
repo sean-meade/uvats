0a0af742d2cb88f107306bb41a27f77e324f203d
Sean Meade <seanpatmeade@gmail.com>
2020-10-03 17:07:43 +0100

    Update. Need to work it out on paper first.

--- uvats.py
+++ uvats.py
@@ -14,11 +14,13 @@ s = 0.5(u + v)t
 
 import math
 
-"""u = int(input("initial velocity: "))
+"""
+u = int(input("initial velocity: "))
 v = int(input("final velocity: "))
 a = int(input("acceleration: "))
 t = int(input("time: "))
-s = int(input("distance: "))"""
+s = int(input("distance: "))
+"""
 
 def solveuvats(u, v, a, t, s):
     # When a, t and s are known.
@@ -28,31 +30,42 @@ def solveuvats(u, v, a, t, s):
         # solve for v
         v = math.sqrt( u ** 2 + 2 * a * s)
 
+    # When a, t and v are known
     elif (u == 0 and a == 0):
         # solve for u
         u = (2 * s - v * t) / t
         # solve for a
         a = ((v**2 - u**2) / (2 * s))
     
-    """elif (u = 0 & t = 0):
+    # When a, v, and s are known
+    elif (u == 0 & t == 0):
+        # solve for u
+        u = math.sqrt((v**2 - (2 * a * s)))
+        # solve for t
+        t = (s/(0.5 * (u + v)))
     
-    elif (u = 0 & s = 0):
+    elif (u == 0 & s == 0):
+        # solve for s
+        s = v * t - 0.5 * (a) * t**2
+        # solve for u
+        u = math.sqrt((v**2 - (2 * a * s)))
         
-    elif (v = 0 & a = 0):
+    elif (v == 0 & a == 0):
+        
 
-    elif (v = 0 & t = 0):
+    """elif (v == 0 & t == 0):
     
-    elif (v = 0 & s = 0):
+    elif (v == 0 & s == 0):
 
-    elif (a = 0 & t = 0):
+    elif (a == 0 & t == 0):
 
-    elif (a = 0 & s = 0):
+    elif (a == 0 & s == 0):
 
-    elif (t = 0 & s = 0):
+    elif (t == 0 & s == 0):
 
     else:
         print("not enough information to solve")"""
 
     print(u, v, a, t, s)
 
-solveuvats(0, 10, 0, 10, 20)
+solveuvats(0, 100, 50, 70, 0)
